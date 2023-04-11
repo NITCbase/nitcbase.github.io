09ee5107573ca47141874370b49af2ee605301b6
Cliford <clifordjo@gmail.com>
2023-04-11 15:53:40 +0530

    update fetch component

--- src/components/FetchCodeBlock.js
+++ src/components/FetchCodeBlock.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 import CodeBlock from "@theme/CodeBlock";
 
 const FetchDisplay = ({ link, language }) => {
-  const [content, setContent] = useState("...");
+  const [content, setContent] = useState(null);
   useEffect(() => {
     fetch(link)
       .then((res) => res.text())
       .then((res) => setContent(res));
   }, []);
-  return <CodeBlock language={language}>{content}</CodeBlock>;
+  return (
+    <CodeBlock language={content ? language : "plain"}>
+      {content || `fetching from ${link} ...`}
+    </CodeBlock>
+  );
 };
 
 export default FetchDisplay;
